ec5619f26e1debcec9f3bf012badfbe7fcc1db3d
sherzo <saulflorez.backend@gmail.com>
2017-09-29 08:14:24 -0430

    Clase #1 area de un triángulo

---
+++ triangulo.js
@@ -0,0 +1,7 @@
+let base = 5
+let height = 7
+
+const triangleArea = (base, height) => base * height / 2
+
+console.log(`El área de un triángulo de base
+${base} y altura ${height} es: ${triangleArea(base, height)}`)
